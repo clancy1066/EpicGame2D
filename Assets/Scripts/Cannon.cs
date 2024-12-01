2b31f76645003aebae78cc0fb47074ba4df0550a
Clancy John Imislund <clancy1066@yahoo.com>
2024-12-01 11:54:15 -0800

    Testing Git 5

--- Assets/Scripts/Cannon.cs
+++ Assets/Scripts/Cannon.cs
@@ -32,7 +32,7 @@ public class Cannon : SpriteAnim
         // TODO
         switch (state)
         {
-            // TODO 3
+            // TODO 2
             case CANNON_STATE.IDLE:     ExecuteIDLE();   break;
             case CANNON_STATE.SHOOT:    ExecuteSHOOT (); break;
             case CANNON_STATE.SMOKE1:   ExecuteSMOKE1(); break;
